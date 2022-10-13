9dc948b7a3a96fd6158eb789a82b7838fc3a707f
Riya505 <riyapk505@gmail.com>
2022-10-13 06:46:20 +0530

    new

---
+++ src/app/view-all-passengers/view-all-passengers.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-all-passengers',
+  templateUrl: './view-all-passengers.component.html',
+  styleUrls: ['./view-all-passengers.component.css']
+})
+export class ViewAllPassengersComponent implements OnInit {
+
+  constructor() { }
+
+  passengersData={}
+
+  ngOnInit(): void {
+  }
+
+}
